75f581d468f93161b611798481dfae7282af2da7
1tsMeRaff <srafi3225@gmail.com>
2026-03-15 01:07:15 +0700

    Initial commit project Java

---
+++ src/main/GameCore.java
@@ -0,0 +1,17 @@
+package main;
+
+public class GameCore {
+	
+	private GameFrame gameFrame;
+	private GamePanel gamePanel;
+	
+	public GameCore() {
+		gamePanel = new GamePanel();
+		gameFrame = new GameFrame(gamePanel);
+		
+		
+		
+		
+	}
+
+}
---
+++ src/main/GameFrame.java
@@ -0,0 +1,18 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class GameFrame {
+	private JFrame Jframe;
+	public GameFrame(GamePanel gamePanel) {
+		
+		Jframe = new JFrame();
+		
+		Jframe.setSize(400, 400);
+		Jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Jframe.add(gamePanel);
+		Jframe.setVisible(true);
+		
+	}
+
+}
